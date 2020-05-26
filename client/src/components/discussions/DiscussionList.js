ed86d426928059c013cb6c510c5e9c26c7617bdd
Neha Brahmankar <nbrahmankar@slack-corp.com>
2020-05-26 19:10:54 +0530

    Lower-casing directory names

---
+++ client/src/components/discussions/DiscussionList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Discussion from './Discussion'
+import { useData } from '../../hooks/Hooks'
+import Spinner from '../Spinner'
+import { motion } from 'framer-motion'
+
+const DiscussionList = () => {
+  const [loadingDiscussions, discussions] = useData(
+    'https://fanas.herokuapp.com/discussions'
+  )
+
+  return !loadingDiscussions ? (
+    <motion.div
+      className="container"
+      initial="hidden"
+      animate="visible"
+      exit
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+      }}
+    >
+      {discussions.map((discussion, i) => (
+        <Discussion discussion={discussion} key={discussion.title + i} />
+      ))}
+    </motion.div>
+  ) : (
+    <Spinner />
+  )
+}
+
+export default DiscussionList
