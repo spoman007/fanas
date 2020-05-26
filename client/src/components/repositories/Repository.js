b08973279a11077a2f5056c81b9a97d0d729e80f
Neha Brahmankar <nbrahmankar@slack-corp.com>
2020-05-26 20:02:35 +0530

    Add missing files

---
+++ client/src/components/repositories/Repository.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Repository = ({
+  repo: { title, link, description, language, stars },
+}) => (
+  <div className="card">
+    <div onClick={() => window.open(link, '_newtab')}>
+      <span>{title}</span>
+      <p>{description}</p>
+      <p>
+        <span className="repo-stats">
+          <span
+            className="repo-language-color"
+            style={{
+              backgroundColor:
+                language === 'JavaScript' ? '#f1e05a' : '#2b7489',
+            }}
+          ></span>
+          <span>{language}</span>
+        </span>
+        <svg
+          height="16"
+          className="octicon octicon-star"
+          aria-label="star"
+          viewBox="0 0 14 16"
+          version="1.1"
+          width="14"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
+          ></path>
+        </svg>
+        {stars}
+      </p>
+    </div>
+  </div>
+)
+
+export default Repository
---
+++ client/src/components/repositories/RepositoryList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Repository from './Repository'
+import Spinner from '../Spinner'
+import { useData } from '../../hooks/Hooks'
+import { motion } from 'framer-motion'
+
+const RepositoryList = () => {
+  const [loadingRepositories, repos] = useData(
+    'https://fanas.herokuapp.com/repositories'
+  )
+
+  return !loadingRepositories ? (
+    <motion.div
+      className="container"
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+      }}
+    >
+      {repos.map((repo) => (
+        <Repository repo={repo} key={repo.title} />
+      ))}
+    </motion.div>
+  ) : (
+    <Spinner />
+  )
+}
+
+export default RepositoryList
